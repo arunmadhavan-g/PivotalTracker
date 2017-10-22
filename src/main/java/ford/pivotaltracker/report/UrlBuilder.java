2bf9ef5b80de419cec4ba7fd35fa2e2bf766e365
arunmadhavan-g <arunmadhavan.g@gmail.com>
2017-10-22 15:57:15 +0530

    handled all the URL styles. handled it as story endpoints rather than search endpoints as they do not have capability to filter based on before and after story ids

--- src/main/java/ford/pivotaltracker/report/UrlBuilder.java
+++ src/main/java/ford/pivotaltracker/report/UrlBuilder.java
@@ -2,16 +2,19 @@ package ford.pivotaltracker.report;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class UrlBuilder {
 
 	private final String baseURL;
 	private String projectId;
-	private List<String> labels = new ArrayList<>();
-
+	private String epic;
+	private int limit = 500;
+	private String beforeId;
+	private String afterId;
+	private String state;
+	
 	public UrlBuilder(String baseURL) {
 		this.baseURL = baseURL;
 	}
@@ -21,32 +24,59 @@ public class UrlBuilder {
 		return this;
 	}
 
-	public UrlBuilder addLabel(String label) {
-		labels.add(label);
+	public UrlBuilder limit(int limit) {
+		this.limit = limit;
+		return this;
+	}
+	public UrlBuilder forEpic(String epic) {
+		this.epic= epic;
+		return this;
+	}
+
+	public UrlBuilder beforeId(String beforeId) {
+		this.beforeId = beforeId;
+		return this;
+	}
+
+	public UrlBuilder afterId(String afterId) {
+		this.afterId = afterId;
+		return this;
+	}
+
+	public UrlBuilder withState(String state) {
+		this.state = state;
 		return this;
 	}
 
 	public String build() throws URISyntaxException, UnsupportedEncodingException {
-		return new StringBuilder(String.format("%s%s/search?", baseURL, projectId))
-					.append("query=")
-					.append(buildQuery(labels))
+		return new StringBuilder(String.format("%s%s/stories?", baseURL, projectId))
+					.append("with_label=")
+					.append(epic)
+					.append(beforeIdQuery())
+					.append(afterIdQuery())
+					.append(stateQuery())
+					.append(limitQuery())
 					.toString();
 	}
 
-	private String buildQuery(List<String> labels) throws UnsupportedEncodingException {
-		StringBuilder builder = new StringBuilder();
-		boolean isFirst = true;
-		for(String label: labels) {
-			if(!isFirst) {
-				builder.append(" AND ");
-			}
-			isFirst = false;
-			builder.append("label:\"")
-					.append(label)
-					.append("\"");
-		}
-		builder.append(" includedone:true");
-		return URLEncoder.encode(builder.toString(),"UTF-8");
+	private String stateQuery() {
+		return returnQueryOrBlank(state, "with_state");
+	}
+
+	private String limitQuery() {
+		return returnQueryOrBlank(Integer.toString(limit), "limit");
+	}
+
+	private String afterIdQuery() {
+		return returnQueryOrBlank(afterId, "after_story_id");
+	}
+
+	private String beforeIdQuery() {
+		return returnQueryOrBlank(beforeId, "before_story_id");
+	}
+
+	private String returnQueryOrBlank(String value, String queryParam) {
+		return StringUtils.isEmpty(value)? "": String.format("&%s=%s",queryParam, value);
 	}
 	
 }
--- src/test/java/ford/pivotaltracker/report/URLBuilderTest.java
+++ src/test/java/ford/pivotaltracker/report/URLBuilderTest.java
@@ -2,29 +2,48 @@ package ford.pivotaltracker.report;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+
 import org.junit.Test;
 
 public class URLBuilderTest {
 
-	private UrlBuilder builder = new UrlBuilder("https://www.pivotaltracker.com/services/v5/projects/");
+	@Test
+	public void forProjectIdAndEpic_GenerateURL() throws Exception {
+		assertThat(baseBuilder().build()).isEqualTo("https://www.pivotaltracker.com/services/v5/projects/1431430/stories?with_label=v1.9&limit=300");
+	}
 	
 	@Test
-	public void forProjectIdAndMainTag_GenerateURL() throws Exception {
-		String url= builder.projectId("1431430")
-						   .addLabel("v1.9")
-						   .build();
-
-		assertThat(url).isEqualTo("https://www.pivotaltracker.com/services/v5/projects/1431430/search?query=label%3Av1.9+includedone%3Atrue");
+	public void forProjectIdAndEpic_WithBeforeId_GenerateUrl() throws Exception {
+		assertThat(baseBuilder()
+							.beforeId("111")
+							.build())
+					.isEqualTo("https://www.pivotaltracker.com/services/v5/projects/1431430/stories?with_label=v1.9&before_story_id=111&limit=300");
 	}
-
+	
 	@Test
-	public void withExtraLabel_queryIsAppendedWithAndLabelCondition() throws Exception {
-		String url= builder.projectId("1431430")
-				   .addLabel("v1.9")
-				   .addLabel("qc pass")
-				   .build();
+	public void forProjectIdAndEpic_WithAfterId_GenerateUrl() throws Exception {
+		assertThat(baseBuilder()
+				.afterId("111")
+				.build())
+				.isEqualTo("https://www.pivotaltracker.com/services/v5/projects/1431430/stories?with_label=v1.9&after_story_id=111&limit=300");
 		
-		assertThat(url).isEqualTo("https://www.pivotaltracker.com/services/v5/projects/1431430/search?query=label%3A%22v1.9%22%20AND%20label%3A%22qc%20pass%22%20includedone%3Atrue%26envelope%3Dtrue");
+	}
+	
+	@Test
+	public void forProjectIdAndEpic_WithSpecificState_GenerateUrl() throws Exception {
+		assertThat(baseBuilder()
+					.withState("accepted")
+					.build())
+					.isEqualTo("https://www.pivotaltracker.com/services/v5/projects/1431430/stories?with_label=v1.9&with_state=accepted&limit=300");
+	}
+	
+	private UrlBuilder baseBuilder() throws URISyntaxException, UnsupportedEncodingException {
+		return new UrlBuilder("https://www.pivotaltracker.com/services/v5/projects/")
+				.projectId("1431430")
+				.forEpic("v1.9")
+				.limit(300);
 	}
 	
 }
